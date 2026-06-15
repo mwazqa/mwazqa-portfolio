ae38215a4aceef2a4da252c652b17c8a7ee77aac
mwazqa <fingeronsmartphone@gmail.com>
2026-06-16 01:50:11 +0200

    ?

--- playwright-tests-ts/playwright.config.ts
+++ playwright-tests-ts/playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   retries: process.env.CI ? 1 : 0,
   workers: process.env.CI ? 2 : undefined,
   use: {
-    headless: true,
+    headless: false,
     baseURL: process.env.API_BASE_URL || 'https://demoqa.com',
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
@@ -16,8 +16,8 @@ export default defineConfig({
   },
   projects: [
     { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
-    { name: 'firefox',  use: { ...devices['Desktop Firefox'] } },
-    { name: 'webkit',   use: { ...devices['Desktop Safari'] } },
+    // { name: 'firefox',  use: { ...devices['Desktop Firefox'] } },
+    // { name: 'webkit',   use: { ...devices['Desktop Safari'] } },
   ],
   reporter: [
     ['list'],
